d2f21e13afd0c90ce522a74b4ca6438b97bcf653
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-25 16:20:35 +0100

    Update Gubu shape for BasicMain

--- src/lib/BasicMain.tsx
+++ src/lib/BasicMain.tsx
@@ -1,59 +1,78 @@
-
 import React, { Fragment } from 'react'
 
 import { useSelector } from 'react-redux'
 
-import {
-  Routes,
-  Route
-} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 import BasicLed from './BasicLed'
-import { Gubu } from 'gubu'
+import { Child, Exact, Gubu } from 'gubu'
 
-
-function makeCmp(view: any, ctx: any) {
+function makeCmp (view: any, ctx: any) {
   let cmp: any = () => <div>NONE</div>
 
   const content = view.content || {}
 
   if ('custom' === content.kind) {
     cmp = ctx().cmp[content.cmp]
-  }
-  else if ('led' === content.kind) {
+  } else if ('led' === content.kind) {
     cmp = BasicLed
   }
 
   return cmp
 }
 
+// Validate spec shape with Gubu
+const BasicMainSpecShape = Gubu({
+  main: {},
+  view: Child({
+    name: String,
+    title: String,
+    icon: String,
+    content: {
+      kind: Exact('led', 'custom'),
+      def: {
+        ent: {
+          primary: {
+            field: {
+              id: {
+                title: String,
+                edit: Boolean
+              }
+            }
+          }
+        },
+        add: {
+          active: Boolean
+        },
+        edit: {
+          layout: {
+            order: String,
+            field: Child({
+              type: String,
+              headerName: String,
+              edit: Boolean,
+              kind: Child({
+                title: String
+              })
+            })
+          }
+        }
+      }
+    }
+  })
+})
 
-
-function BasicMain(props: any) {
-  const {
-    vxg,
-    ctx,
-    spec
-  } = props
+function BasicMain (props: any) {
+  const { vxg, ctx } = props
   const { model, content } = ctx()
 
-  const { frame } = spec
+  const basicMainSpec = BasicMainSpecShape(props.spec)
 
-  // spec schema definition with Gubu
-  const shape = Gubu({
-    main: {},
-    view: {}
-  })
+  const views = Object.values(basicMainSpec.view)
 
-  // spec schema validation with Gubu
-  shape(spec)
-
-  const part = spec.main
-
-  const views = Object.values(spec.view)
-
-  const sideOpen = useSelector((state: any) => state.main.vxg.cmp.BasicSide.show)
-
+  const sideOpen = useSelector(
+    (state: any) => state.main.vxg.cmp.BasicSide.show
+  )
 
   const divStyle = {
     marginLeft: '2em',
@@ -71,31 +90,32 @@ function BasicMain(props: any) {
       <div style={{ ...divStyle }}>
         <Routes>
           <Route path='/view'>
-            {
-              views.map((view: any) => {
-                const Cmp: any = makeCmp(view, ctx)
-                if (view.paramId) {
-                  return (
-                    <Fragment key={view.name}>
-                      <Route
-                        key={view.name}
-                        path={'/view/' + view.name}
-                        element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
-                      />
-                      <Route
-                        key={view.name}
-                        path={'/view/' + view.name + '/:' + view.paramId}
-                        element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
-                      />
-                    </Fragment>)
-                }
-                return <Route
+            {views.map((view: any) => {
+              const Cmp: any = makeCmp(view, ctx)
+              if (view.paramId) {
+                return (
+                  <Fragment key={view.name}>
+                    <Route
+                      key={view.name}
+                      path={'/view/' + view.name}
+                      element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
+                    />
+                    <Route
+                      key={view.name}
+                      path={'/view/' + view.name + '/:' + view.paramId}
+                      element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
+                    />
+                  </Fragment>
+                )
+              }
+              return (
+                <Route
                   key={view.name}
                   path={'/view/' + view.name}
                   element={<Cmp vxg={vxg} ctx={ctx} spec={view} />}
                 />
-              })
-            }
+              )
+            })}
           </Route>
         </Routes>
       </div>
