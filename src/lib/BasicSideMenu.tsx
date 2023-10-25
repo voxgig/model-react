af1b42fb77fb538a66e2e506665348d48ef5e09b
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-25 12:28:55 +0100

    Add Gubu to BasicSideMenu and BasicSideMenuItem

--- src/lib/BasicSideMenu.tsx
+++ src/lib/BasicSideMenu.tsx
@@ -1,31 +1,44 @@
+import { Child, Gubu } from 'gubu'
 import BasicSideMenuItem from './BasicSideMenuItem'
 
-function BasicSideMenu(props: any) {
-    const {
-        onItemSelect,
-        spec
-    } = props
+// spec schema definition with Gubu
+const BasicSideMenuSpecShape = Gubu({
+  section: Child({
+    title: String,
+    item: Child({
+      kind: String,
+      label: String,
+      icon: String,
+      path: String,
+      access: Child(Boolean, {})
+    })
+  })
+})
 
-    return (
-        <>
-            {
-                Object.entries(spec.sectionList).map(([sectionKey, section]: [any, any]) => {
-                    // TODO: do we need to pass viewPath?
-                    const basicSideMenuItemSpec = {
-                        section: section
-                    }
-                    return (
-                        <BasicSideMenuItem key={sectionKey} spec={basicSideMenuItemSpec} onItemSelect={onItemSelect} />
-                    )
-                })
-            }
-        </>
-    )
+function BasicSideMenu (props: any) {
+  const { onItemSelect } = props
+
+  const basicSideMenuSpec = BasicSideMenuSpecShape(props.spec)
+
+  return (
+    <>
+      {Object.entries(basicSideMenuSpec.section).map(
+        ([sectionKey, section]: [any, any]) => {
+          // TODO: do we need to pass viewPath?
+          const basicSideMenuItemSpec = {
+            section
+          }
+          return (
+            <BasicSideMenuItem
+              key={sectionKey}
+              spec={basicSideMenuItemSpec}
+              onItemSelect={onItemSelect}
+            />
+          )
+        }
+      )}
+    </>
+  )
 }
 
 export default BasicSideMenu
-
-
-
-
-
--- src/lib/BasicSideMenuItem.tsx
+++ src/lib/BasicSideMenuItem.tsx
@@ -1,44 +1,52 @@
-import { Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from '@mui/material'
 
 import {
-    ChatBubble as ChatBubbleIcon,
-    FactoryOutlined,
-    KeyOutlined,
-    AssignmentTurnedInOutlined,
-    TextSnippetOutlined,
-    HighlightAlt,
-    SupervisorAccount as AccountIcon,
-    Map as MapIcon,
-    Tablet as TabletIcon,
-    Update as UpdateIcon,
-    Security as AdminIcon,
-    ContentPaste as ClipBoardIcon,
-    FitScreen,
-    Event as EventIcon,
-    Logout as LogoutIcon
+  ChatBubble as ChatBubbleIcon,
+  FactoryOutlined,
+  KeyOutlined,
+  AssignmentTurnedInOutlined,
+  TextSnippetOutlined,
+  HighlightAlt,
+  SupervisorAccount as AccountIcon,
+  Map as MapIcon,
+  Tablet as TabletIcon,
+  Update as UpdateIcon,
+  Security as AdminIcon,
+  ContentPaste as ClipBoardIcon,
+  FitScreen,
+  Event as EventIcon,
+  Logout as LogoutIcon
 } from '@mui/icons-material'
+import { Child, Gubu } from 'gubu'
 
 const iconmap: any = {
-    factory: FactoryOutlined,
-    key: KeyOutlined,
-    done: AssignmentTurnedInOutlined,
-    docs: TextSnippetOutlined,
-    hightlight: HighlightAlt,
-    map: MapIcon,
-    account: AccountIcon,
-    tablet: TabletIcon,
-    update: UpdateIcon,
-    admin: AdminIcon,
-    clipboard: ClipBoardIcon,
-    fitscreen: FitScreen,
-    chatBubble: ChatBubbleIcon,
-    event: EventIcon,
-    logout: LogoutIcon
+  factory: FactoryOutlined,
+  key: KeyOutlined,
+  done: AssignmentTurnedInOutlined,
+  docs: TextSnippetOutlined,
+  hightlight: HighlightAlt,
+  map: MapIcon,
+  account: AccountIcon,
+  tablet: TabletIcon,
+  update: UpdateIcon,
+  admin: AdminIcon,
+  clipboard: ClipBoardIcon,
+  fitscreen: FitScreen,
+  chatBubble: ChatBubbleIcon,
+  event: EventIcon,
+  logout: LogoutIcon
 }
 
-function makeIcon(name: string) {
-    const Icon = iconmap[name]
-    return <Icon />
+function makeIcon (name: string) {
+  const Icon = iconmap[name]
+  return <Icon />
 }
 
 // TODO: Grab the role from the redux store
@@ -46,41 +54,58 @@ const userRole = 'admin'
 
 // TODO: move to utils
 // TODO: allow custom authorization function
-function isAuthorized(userRole: string, authorizedRoles: any): boolean {
-    return authorizedRoles.hasOwnProperty(userRole) && authorizedRoles[userRole] === true
+function isAuthorized (userRole: string, authorizedRoles: any): boolean {
+  return (
+    authorizedRoles.hasOwnProperty(userRole) &&
+    authorizedRoles[userRole] === true
+  )
 }
 
-function BasicSideMenuItem(props: any) {
-    const { spec, sectionKey, onItemSelect } = props
+// spec schema definition with Gubu
+const BasicSideMenuItemSpecShape = Gubu({
+  section: Child({
+    title: String,
+    item: Child({
+      kind: String,
+      label: String,
+      icon: String,
+      path: String,
+      access: Child(Boolean, {})
+    })
+  })
+})
 
-    const viewPath: any = location.pathname.split('/')[2]
+function BasicSideMenuItem (props: any) {
+  const { sectionKey, onItemSelect } = props
 
-    return (
-        <List key={sectionKey}>
-            {
-                Object.entries(spec.section.item).map(([itemKey, item]: [any, any]) => {
-                    return (
-                        // TODO: load user from redux store
-                        isAuthorized('admin', item.access) && (
-                            <ListItem
-                                key={itemKey}
-                                disablePadding
-                                onClick={() => onItemSelect(itemKey, item)}
-                            >
-                                <ListItemButton selected={viewPath == itemKey}>
-                                    <ListItemIcon>
-                                        {makeIcon(item.icon)}
-                                    </ListItemIcon>
-                                    <ListItemText primary={item.label} />
-                                </ListItemButton>
-                            </ListItem>
-                        )
-                    )
-                })
-            }
-            <Divider />
-        </List>
-    )
+  const viewPath: any = location.pathname.split('/')[2]
+
+  const basicSideMenuItemSpec = BasicSideMenuItemSpecShape(props.spec)
+
+  return (
+    <List key={sectionKey}>
+      {Object.entries(basicSideMenuItemSpec.section.item).map(
+        ([itemKey, item]: [any, any]) => {
+          return (
+            // TODO: load user from redux store
+            isAuthorized('admin', item.access) && (
+              <ListItem
+                key={itemKey}
+                disablePadding
+                onClick={() => onItemSelect(itemKey, item)}
+              >
+                <ListItemButton selected={viewPath == itemKey}>
+                  <ListItemIcon>{makeIcon(item.icon)}</ListItemIcon>
+                  <ListItemText primary={item.label} />
+                </ListItemButton>
+              </ListItem>
+            )
+          )
+        }
+      )}
+      <Divider />
+    </List>
+  )
 }
 
 export default BasicSideMenuItem
