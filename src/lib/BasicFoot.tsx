d7e52e2f70ff769c062ea123a5a6ac17c54d3edc
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-25 16:44:18 +0100

    Update Gubu shape on BasicFoot

--- src/lib/BasicFoot.tsx
+++ src/lib/BasicFoot.tsx
@@ -1,36 +1,38 @@
+import { Box, Container, Grid, Typography } from '@mui/material'
+import { Gubu } from 'gubu'
 
-import React from 'react'
+// Validate spec shape with Gubu
+const BasicFootSpecShape = Gubu({
+  foot: {
+    title: String
+  },
+  view: {}
+})
 
-import { Box, Container, Grid, Typography } from "@mui/material"
-
-
-function BasicFoot(props: any) {
-  const {
-    vxg,
-    ctx,
-    spec
-  } = props
+function BasicFoot (props: any) {
+  const { vxg, ctx } = props
   const model = ctx().model
 
-  const { frame } = spec
+  const basicFootSpec = BasicFootSpecShape(props.spec)
 
-  const part = spec.foot
+  console.log('basicFootSpec: ', basicFootSpec)
+
+  const part = basicFootSpec.foot
 
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "auto",
-        paddingTop: "0.5rem",
-        paddingBottom: "0.5rem",
-        borderTop: '1px solid #CCC',
-
+        width: '100%',
+        height: 'auto',
+        paddingTop: '0.5rem',
+        paddingBottom: '0.5rem',
+        borderTop: '1px solid #CCC'
       }}
     >
-      <Container maxWidth="lg">
-        <Grid container direction="column">
+      <Container maxWidth='lg'>
+        <Grid container direction='column'>
           <Grid item xs={12} sx={{ textAlign: 'right' }}>
-            <Typography color="#CCC" variant="body2">
+            <Typography color='#CCC' variant='body2'>
               {part.title}
             </Typography>
           </Grid>
