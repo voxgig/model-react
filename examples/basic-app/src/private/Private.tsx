8f2608933074cf3d68d1d61f2560b4c030b86a97
Zack Braksa <zakaria.braksa@gmail.com>
2023-10-26 13:59:53 +0100

    Add custom Chat component

--- examples/basic-app/src/private/Private.tsx
+++ examples/basic-app/src/private/Private.tsx
@@ -1,17 +1,13 @@
-
 import React from 'react'
 
-
 import { useSelector } from 'react-redux'
-
 
 import { BasicAdmin } from '@voxgig/model-react'
 
 import { getMain } from '../setup'
-
+import Chat from './Chat'
 
 const main = getMain()
-
 
 // Provided as a function to prevent deep inspection.
 const ctx = () => ({
@@ -19,30 +15,25 @@ const ctx = () => ({
   seneca: main.seneca,
   store: main.store,
   cmp: {
+    chat: Chat
   },
   custom: {
     BasicLed: {
-      query: (view: any, cmpstate: any) => { }
+      query: (view: any, cmpstate: any) => {}
     }
   }
 })
-
 
 const spec = {
   frame: 'private'
 }
 
-
-function Private(props: any) {
-
+function Private (props: any) {
   return (
-    <div className="Private">
+    <div className='Private'>
       <BasicAdmin ctx={ctx} spec={spec} />
     </div>
   )
 }
 
-
-
 export default Private
-
