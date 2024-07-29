0ca672d5733b4fa63661092b9a2ed8859107c11e
Zack Braksa <zack.braksa@voxgig.com>
2024-07-29 20:54:30 +0100

    Refactor BasicEntityRadioGroupField to make use of field.cat and resolveCategories and resolveValue

--- src/lib/BasicEntityRadioGroupField.tsx
+++ src/lib/BasicEntityRadioGroupField.tsx
@@ -6,6 +6,7 @@ import { Controller } from 'react-hook-form'
 import type { Spec } from './basic-types'
 
 import { Default, Exact, Gubu, Skip } from 'gubu'
+import { resolve } from 'path'
 const CMPNAME = 'BasicEntitySliderField'
 
 const { Open } = Gubu
@@ -21,25 +22,30 @@ const BasicEntityRadioGroupFieldSpecShape = Gubu(
         edit: Default(true),
         direction: Exact('row', 'column').Default('row'),
       }),
-      options: Open({
-        label: { field: Default('label') },
-        value: { field: Default('value') },
-        default: Open({}),
-        ents: Open({}),
+      cat: Open({
+        default: '',
+        title: String,
+        multiple: Number,
+        order: {
+          sort: '',
+          exclude: '',
+          include: '',
+        },
+        item: Open({}),
       }),
     }),
   }),
   { name: CMPNAME }
 )
 
-function BasicEntityRadioGroupField(props: any) {
+function BasicEntityRadioGroupField (props: any) {
   const { spec } = props
 
   const basicEntityRadioGroupField: Spec =
     BasicEntityRadioGroupFieldSpecShape(spec)
   const { control, field } = basicEntityRadioGroupField
 
-  const { resolvedOptions, resolvedDefault } = resolveOptions(field.options)
+  const { resolvedCategories, resolvedDefault } = resolveCategories(field.cat)
 
   return (
     <>
@@ -52,18 +58,18 @@ function BasicEntityRadioGroupField(props: any) {
         render={({ field: { onChange, value } }) => (
           <RadioGroup
             key={field.id}
-            value={value}
+            value={resolveValue(value, field.cat)}
             onChange={onChange}
             row={'row' === field.ux.direction}
             disabled={!field.ux.edit}
             {...field.ux.props}
           >
-            {resolvedOptions.map((option: any) => (
+            {resolvedCategories.map((option: any) => (
               <FormControlLabel
-                key={`${option.value}-option`}
-                value={option.value}
+                key={`${option.key}-option`}
+                value={option.key}
                 control={<Radio disabled={!field.ux.edit} />}
-                label={option.label}
+                label={option.title}
               />
             ))}
           </RadioGroup>
@@ -73,39 +79,77 @@ function BasicEntityRadioGroupField(props: any) {
   )
 }
 
+// TODO: Make it DRY
 // Returns array of options and default value(s) based on the options object
-function resolveOptions(options: any) {
-  const { multiple, ents, label, value, default: defaultValues } = options
-  const labelField = label?.field
-  const valueField = value?.field
+function resolveCategories (cat: any) {
+  const { multiple, item: items, default: defaultValues } = cat
+
+  // console.log('resolveCat', 'cat', cat)
 
   // Array of options
-  const resolvedOptions = Object.keys(ents).map((key) => ({
-    [labelField]: ents?.[key]?.[labelField],
-    [valueField]: key,
+  const resolvedCategories = Object.keys(items).map((key) => ({
+    title: items?.[key]?.title,
+    key: key,
   }))
 
-  let resolvedDefault
-  if (multiple === false) {
-    if (Object.keys(defaultValues).length > 0) {
-      const firstKey = Object.keys(defaultValues)[0]
-      resolvedDefault = {
-        value: firstKey,
-        label: defaultValues[firstKey][labelField],
-      }
-    } else {
-      resolvedDefault = null
-    }
-  } else {
-    resolvedDefault = Object.keys(defaultValues).map((key) => ({
-      label: defaultValues[key].label,
-      value: key,
-    }))
+  let resolvedDefault: any = multiple === 1 ? null : []
+  let defaultList: any[] = []
+
+  if (typeof defaultValues === 'string') {
+    defaultList = defaultValues.split(',')
   }
 
+  const mapResolvedDefault = (list: any[]) =>
+    list.map((val: any) => ({
+      title: items[val].title,
+      key: val,
+    }))
+
+  if (multiple === 1) {
+    resolvedDefault = {
+      title: items[defaultList[0]].title,
+      key: defaultList[0],
+    }
+  } else if (multiple === -1) {
+    resolvedDefault = mapResolvedDefault(defaultList)
+  } else if (multiple > 1) {
+    resolvedDefault = mapResolvedDefault(defaultList.slice(0, multiple))
+  }
+
+  // console.log('resolveCat', 'resolvedDefault', resolvedDefault)
+
   return {
-    resolvedOptions,
+    resolvedCategories,
     resolvedDefault,
+  }
+}
+
+// TODO: Make it DRY
+function resolveValue (
+  value: any,
+  cat: {
+    multiple: number
+    item: Record<string, { title: string }>
+  }
+) {
+  const { item: items, multiple } = cat
+
+  if (Array.isArray(value)) {
+    return value
+  }
+
+  switch (multiple) {
+    case 1:
+      return { key: value, title: items[value]?.title }
+    case -1:
+      return value
+        .split(',')
+        .map((k: any) => ({ key: k, title: items[k]?.title }))
+    default:
+      return value
+        .split(',')
+        .slice(0, multiple)
+        .map((k: any) => ({ key: k, title: items[k]?.title }))
   }
 }
 
