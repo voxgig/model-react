b4f6f49636da0f836203d3dcb5de12402f99f957
Zack Braksa <zack.braksa@voxgig.com>
2024-07-30 20:03:09 +0100

    Refactor BasicEntityAutocompleteField to include error handling and improve code readability

--- src/lib/BasicEntityAutocompleteField.tsx
+++ src/lib/BasicEntityAutocompleteField.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, forwardRef } from 'react'
 
-import { TextField, Autocomplete } from '@mui/material'
+import { TextField, Autocomplete, Box } from '@mui/material'
 import { Controller } from 'react-hook-form'
 
 import type { Spec } from './basic-types'
 
 import { Default, Exact, Gubu } from 'gubu'
+import { BasicEntityFieldError } from './BasicEntityFieldError'
 const CMPNAME = 'BasicEntityAutocompleteField'
 
 const { Open } = Gubu
@@ -40,34 +41,39 @@ function BasicEntityAutocompleteField (props: any) {
   const { spec } = props
 
   const basicEntityAutocompleteField: Spec = BasicEntityAutocompleteFieldSpecShape(spec)
-  const { control, field } = basicEntityAutocompleteField
+  const { control, field, errors } = basicEntityAutocompleteField
+
+  const err = errors[field.name]
 
   return (
-    <Controller
-      key={`${field.id}-controller`}
-      name={field.name}
-      control={control}
-      defaultValue={resolveDefault(field.cat)}
-      render={({ field: { onChange, value } }) => (
-        <Autocomplete
-          freeSolo
-          forcePopupIcon
-          multiple={field.cat.multiple !== 1}
-          options={resolveCategories(field.cat)}
-          isOptionEqualToValue={(opt: any, val: any) =>
-            opt === val ||
-            (opt?.id != null && val?.id != null && opt.id === val.id) ||
-            (opt?.key != null && val?.key != null && opt.key === val.key)
-          }
-          getOptionLabel={(option: any) => option.title}
-          value={resolveValue(value, field.cat)}
-          disabled={!field.ux.edit}
-          {...field.ux.props}
-          onChange={(_, newVal: any) => onChange(newVal)}
-          renderInput={(params: any) => <TextField {...params} label={field.label} />}
-        />
-      )}
-    />
+    <Box key={`${field.id}-box`}>
+      <Controller
+        key={`${field.id}-controller`}
+        name={field.name}
+        control={control}
+        defaultValue={resolveDefault(field.cat)}
+        render={({ field: { onChange, value } }) => (
+          <Autocomplete
+            freeSolo
+            forcePopupIcon
+            multiple={field.cat.multiple !== 1}
+            options={resolveCategories(field.cat)}
+            isOptionEqualToValue={(opt: any, val: any) =>
+              opt === val ||
+              (opt?.id != null && val?.id != null && opt.id === val.id) ||
+              (opt?.key != null && val?.key != null && opt.key === val.key)
+            }
+            getOptionLabel={(option: any) => option.title}
+            value={resolveValue(value, field.cat)}
+            disabled={!field.ux.edit}
+            {...field.ux.props}
+            onChange={(_, newVal: any) => onChange(newVal)}
+            renderInput={(params: any) => <TextField {...params} label={field.label} />}
+          />
+        )}
+      />
+      <BasicEntityFieldError err={err} />
+    </Box>
   )
 }
 
