4cf008bb926db4da3250e496985e30abfa28af67
Zack Braksa <zack.braksa@voxgig.com>
2024-07-30 13:44:13 +0100

    Debug and refactor BasicEntitySelectField

--- src/lib/BasicEntitySelectField.tsx
+++ src/lib/BasicEntitySelectField.tsx
@@ -40,21 +40,14 @@ function BasicEntitySelectField (props: any) {
   const { spec } = props
 
   const basicEntitySelectField: Spec = BasicEntitySelectFieldSpecShape(spec)
-  const { control, field, getValues } = basicEntitySelectField
-  const { resolvedCategories, resolvedDefault } = resolveCategories(field.cat)
-  const val = getValues(field.name)
-
-  // console.log('field', field)
-  // console.log('val', val)
-  // console.log('resolvedDefault', resolvedDefault)
-  // console.log('resolvedCategories', resolvedCategories)
+  const { control, field } = basicEntitySelectField
 
   return (
     <Controller
       key={`${field.id}-controller`}
       name={field.name}
       control={control}
-      defaultValue={resolvedDefault}
+      defaultValue={resolveDefault(field.cat)}
       render={({ field: { onChange, value } }) => (
         <FormControl fullWidth>
           <InputLabel id={`${field.id}-label`}>{field.label}</InputLabel>
@@ -62,13 +55,13 @@ function BasicEntitySelectField (props: any) {
             labelId={`${field.id}-label`}
             id={`${field.id}-select`}
             value={resolveValue(value, field.cat)}
-            multiple={field.cat.multiple === 1 ? false : true}
+            multiple={field.cat.multiple !== 1}
             label={field.name}
             onChange={(event: any) => onChange(event.target.value)}
             disabled={!field.ux.edit}
             {...field.ux.props}
           >
-            {resolvedCategories.map((opt: any) => (
+            {resolveCategories(field.cat).map((opt: any) => (
               <MenuItem key={opt.key} value={opt.key}>
                 {opt.title}
               </MenuItem>
@@ -80,48 +73,32 @@ function BasicEntitySelectField (props: any) {
   )
 }
 
-// TODO: Make it DRY
-// Returns array of options and default value(s) based on the options object
+// Returns array of options based on field.cat object
 function resolveCategories (cat: any) {
-  const { multiple, item: items, default: defaultValues } = cat
-
-  // console.log('resolveCat', 'cat', cat)
-
-  // Array of options
-  const resolvedCategories = Object.keys(items).map((key) => ({
-    title: items?.[key]?.title,
+  return Object.keys(cat.item).map((key) => ({
+    title: cat.item?.[key]?.title,
     key: key,
   }))
+}
 
-  let resolvedDefault: any = multiple === 1 ? null : []
-  let defaultList: any[] = []
+function resolveDefault (cat: any) {
+  const { multiple, default: defaultValues } = cat
 
-  if (typeof defaultValues === 'string') {
-    defaultList = defaultValues.split(',')
+  if (!defaultValues) {
+    return multiple === 1 ? '' : ['']
   }
 
-  const mapResolvedDefault = (list: any[]) =>
-    list.map((val: any) => ({
-      title: items[val].title,
-      key: val,
-    }))
+  const defaultItems = defaultValues.split(',')
 
-  if (multiple === 1) {
-    resolvedDefault = {
-      title: items[defaultList[0]].title,
-      key: defaultList[0],
-    }
-  } else if (multiple === -1) {
-    resolvedDefault = mapResolvedDefault(defaultList)
-  } else if (multiple > 1) {
-    resolvedDefault = mapResolvedDefault(defaultList.slice(0, multiple))
-  }
+  const mapResolvedDefault = (list: string[]) => list.map((val) => val)
 
-  // console.log('resolveCat', 'resolvedDefault', resolvedDefault)
-
-  return {
-    resolvedCategories,
-    resolvedDefault,
+  switch (multiple) {
+    case 1:
+      return defaultItems.length > 0 ? defaultItems[0] : ''
+    case -1:
+      return mapResolvedDefault(defaultItems)
+    default:
+      return mapResolvedDefault(defaultItems.slice(0, multiple))
   }
 }
 
@@ -130,24 +107,26 @@ function resolveValue (
   value: any,
   cat: { multiple: number; item: Record<string, { title: string }> }
 ) {
-  const { item: items, multiple } = cat
+  const { multiple } = cat
 
   if (Array.isArray(value)) {
-    return value
+    const items = value.map((val) => val.key)
+    return multiple === 1 ? items[0] : items.slice(0, multiple)
   }
 
+  if (typeof value === 'object' && value !== null) {
+    return multiple === 1 ? value.key : [value.key]
+  }
+
+  let items = value.split(',')
   switch (multiple) {
     case 1:
-      return { key: value, title: items[value]?.title }
+      return items[0] || ''
     case -1:
-      return value.split(',')
+      return items
     default:
-      if (cat.multiple > 1) {
-        return value.split(',').slice(0, multiple)
-      }
+      return items.slice(0, multiple)
   }
-
-  return value
 }
 
 export { BasicEntitySelectField }
