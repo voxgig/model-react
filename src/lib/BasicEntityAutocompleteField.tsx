25358fe99e8974be2bdef7d806c1cb3dd3d85564
Zack Braksa <zack.braksa@voxgig.com>
2024-08-02 11:09:35 +0100

    Remove unnecessary code from BasicEntityAutocompleteField

--- src/lib/BasicEntityAutocompleteField.tsx
+++ src/lib/BasicEntityAutocompleteField.tsx
@@ -84,46 +84,6 @@ function BasicEntityAutocompleteField (props: any) {
   )
 }
 
-// function BasicEntityAutocompleteField (props: any) {
-//   const { spec } = props
-
-//   const basicEntityAutocompleteField: Spec = BasicEntityAutocompleteFieldSpecShape(spec)
-//   const { control, field, errors } = basicEntityAutocompleteField
-
-//   const err = errors[field.name]
-
-//   return (
-//     <Box key={`${field.id}-box`}>
-//       <Controller
-//         key={`${field.id}-controller`}
-//         name={field.name}
-//         control={control}
-//         defaultValue={resolveDefault(field.cat)}
-//         render={({ field: { onChange, value } }) => (
-//           <Autocomplete
-//             freeSolo
-//             forcePopupIcon
-//             multiple={field.cat.multiple !== 1}
-//             options={resolveCategories(field.cat)}
-//             isOptionEqualToValue={(opt: any, val: any) =>
-//               opt === val ||
-//               (opt?.id != null && val?.id != null && opt.id === val.id) ||
-//               (opt?.key != null && val?.key != null && opt.key === val.key)
-//             }
-//             getOptionLabel={(option: any) => option.title}
-//             value={resolveValue(value, field.cat)}
-//             disabled={!field.ux.edit}
-//             {...field.ux.props}
-//             onChange={(_, newVal: any) => onChange(newVal)}
-//             renderInput={(params: any) => <TextField {...params} label={field.label} />}
-//           />
-//         )}
-//       />
-//       <BasicEntityFieldError err={err} />
-//     </Box>
-//   )
-// }
-
 export function resolveCategories (cat: {
   item: Record<string, { title: string }>
 }) {
